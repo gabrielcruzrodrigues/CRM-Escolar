e87c157250a54d4813fa6485bc53ce348da9fa52
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-10-17 16:58:04 -0300

    feat: create reactive forms for create-student page

--- CRM-Client/src/app/components/create-students/create-students.component.ts
+++ CRM-Client/src/app/components/create-students/create-students.component.ts
@@ -1,16 +1,67 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../layout/menu/menu.component';
 import { InfosBackComponent } from '../layout/infos-back/infos-back.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-students',
   standalone: true,
-  imports: [MenuComponent, InfosBackComponent],
+  imports: [MenuComponent, InfosBackComponent, ReactiveFormsModule],
   templateUrl: './create-students.component.html',
   styleUrl: './create-students.component.scss'
 })
 export class CreateStudentsComponent {
-
   link: string = '/students-panel';
   title: string = "Cadastrar medicamento";
+  studentForm: FormGroup;
+  schoolForm: FormGroup;
+  responsibleForm: FormGroup;
+  medicationForm: FormGroup;
+  illnessForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) 
+  {
+    this.studentForm = this.fb.group({
+      name: ['', Validators.required],
+      cpf: [''],
+      emergencePhone: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      address: ['', Validators.required],
+      serie: ['', Validators.required],
+      registerValue: ['', Validators.required],
+      paymentDay: ['', Validators.required],
+      schoolId: [''],
+      responsibleId: ['']
+    });
+
+    this.schoolForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: [''],
+      address: ['']
+    });
+
+    this.responsibleForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      type: ['', Validators.required],
+      cpf: ['', Validators.required],
+      address: ['', Validators.required],
+      email: ['']
+    });
+
+    this.medicationForm = this.fb.group({
+      name: ['', Validators.required],
+      times: ['', Validators.required],
+      dosage: ['', Validators.required],
+      studentId: ['', Validators.required]
+    });
+
+    this.illnessForm = this.fb.group({
+      name: ['', Validators.required],
+      observation: [''],
+      studentId: ['', Validators.required]
+    })
+  }
 }
